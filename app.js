5e0bd172b2c0e470f984924bb844a5a0ebaa2016
Jackiegonca <goncalvescaroj@gmail.com>
2025-03-08 23:12:49 -0400

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function agregarAmigo(){
+
+    // var friend = document.getElementById("amigo").textContent
+    console.log("Hola")
+}
+
+
+function sortearAmigo(){
+    
+}
