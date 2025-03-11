23f95466855d9fb388006f1e95fd2afe35eedb84
Jackiegonca <goncalvescaroj@gmail.com>
2025-03-10 21:42:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+
+var inputAmigo = document.getElementById("amigo");
+
+inputAmigo.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      agregarAmigo();
+    }
+});
+
+
 function agregarAmigo(){
 
-    // var friend = document.getElementById("amigo").textContent
-    console.log("Hola")
+    var friend = document.getElementById("amigo");
+    var friendValue = friend.value.trim(); 
+
+    if (friendValue === "") { 
+        console.log("Amigo vacío");
+        alert("Por favor, inserte un nombre.");
+        friend.value = "";
+        return;
+    }
+
+    if (!isNaN(friendValue)) { 
+        console.log("Amigo es un número");
+        alert("¡Introduzca un nombre válido!");
+        friend.value = "";
+        return;
+    }
+
+    let lista = document.getElementById("listaAmigos");
+    let li = document.createElement("li");
+    li.textContent = friendValue;
+    lista.appendChild(li);
+    friend.value = "";
 }
 
 
 function sortearAmigo(){
-    console.log("Hola mundo")
+    
+    listaAmigos = document.querySelectorAll("#listaAmigos li");
+
+    if (listaAmigos.length === 0) {
+        alert("No tienes amigos!")
+        console.error("No se encontraron elementos con el selector proporcionado.");
+        return null;
+    
+    }
+
+    let numeroAleatoreo = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSecreto = listaAmigos[numeroAleatoreo];
+
+    resultado = document.getElementById("resultado");
+    resultado.textContent = "Amigo secreto: " + amigoSecreto.textContent;
+
 }
